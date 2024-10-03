4cc2b52899f4839f7f2c9f29a46b02090a3d80f9
Stephen DeLorme <stephen@d.elor.me>
2024-10-03 12:43:11 -0400

    rough ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,36 @@
 "use client";
 
 import { FedimintWallet } from "@fedimint/core-web";
-import { useEffect, useState } from "react";
+import { ChangeEventHandler, useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [balance, setBalance] = useState(0);
+  const [federationInvite, setFederationInvite] = useState('fed11qgqzc2nhwden5te0vejkg6tdd9h8gepwvejkg6tdd9h8garhduhx6at5d9h8jmn9wshxxmmd9uqqzgxg6s3evnr6m9zdxr6hxkdkukexpcs3mn7mj3g5pc5dfh63l4tj6g9zk4er');
+  const [sendAmount, setSendAmount] = useState(100);
+  const [receiveAmount, setReceiveAmount] = useState(200);
+  const [invoiceToPay, setInvoiceToPay] = useState<string>('')
+  const [notesToSend, setNotesToSend] = useState<string|undefined>('')
+  const [wallet, setWallet] = useState<FedimintWallet|null>()
+  const [notesToReceive, setNotesToReceive] = useState<string|undefined>('')
+  const [invoiceForReceive, setInvoiceForReceive] = useState<string|undefined>('')
 
   async function launchWallet(){
-    console.log('starting wallet')
+    console.log('Starting ATL BitLab Wallet...')
 
     // Create the Wallet client
-    const wallet = new FedimintWallet()
+    const wallet = new FedimintWallet();
+    setWallet(wallet);
 
-    // globalThis.wallet = wallet
+    // Uncomment to access the Wallet client from javascript console
+    globalThis.wallet = wallet
 
     // Open the wallet (should be called once in the application lifecycle)
     await wallet.open()
 
     // Join a Federation (if not already open)
     if (!wallet.isOpen()) {
-      const inviteCode = 'fed11qgqzc2nhwden5te0vejkg6tdd9h8gepwvejkg6tdd9h8garhduhx6at5d9h8jmn9wshxxmmd9uqqzgxg6s3evnr6m9zdxr6hxkdkukexpcs3mn7mj3g5pc5dfh63l4tj6g9zk4er'
-      await wallet.joinFederation(inviteCode)
+      await wallet.joinFederation(federationInvite)
     }
 
     // Get Wallet Balance
@@ -29,35 +39,57 @@ export default function Home() {
     // Subscribe to Balance Updates
     const unsubscribe = wallet.balance.subscribeBalance((balance: number) => {
       console.log('Updated balance:', balance)
+      setBalance(balance);
     })
     // Remember to call unsubscribe() when done
 
-    // Receive Ecash Payments
-    // await wallet.mint.reissueNotes('A11qgqpw9thwvaz7t...')
-
-    // await wallet.mint.redeemEcash('asfsdf')
-
-    
-
-    const spend = await wallet.mint.spendNotes(100)
-    console.log(spend)
-
-    await wallet.mint.redeemEcash(spend.notes)
-
-
-
-    // Create Lightning Invoice
-    // let invoice = await wallet.lightning.createInvoice(10_000, 'description')
-
-    // console.log(invoice)
-
-    // console.log(invoice.invoice)
-
-    // Pay Lightning Invoice
-    // await wallet.lightning.payBolt11Invoice('lnbc...')
-
     unsubscribe();
     
+  }
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    setSendAmount(parseInt(e.target.value));
+  }
+
+  const handleReceiveAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    setReceiveAmount(parseInt(e.target.value));
+  }
+
+  const handleInvoiceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    setInvoiceToPay(e.target.value);
+  }
+
+  const handlesNotesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    setNotesToReceive(e.target.value);
+  }
+
+  const handleMakePayment = async () => {
+    if(invoiceToPay !== '') {
+      console.log('make a lightning payment');
+      await wallet?.lightning.payInvoice(invoiceToPay);
+    }
+    else {
+      console.log('make an ecash payment');
+      let notes = await wallet?.mint.spendNotes(sendAmount);
+      setNotesToSend(notes?.notes);
+    }
+  }
+
+  const handleReceivePayment = async () => {
+    if(notesToReceive !== undefined) {
+      console.log('receive an ecash payment');
+      let notes = await wallet?.mint.redeemEcash(notesToReceive);
+      console.log(notes);
+    }
+  }
+
+  const handleGetInvoice = async () => {
+    let invoice = await wallet?.lightning.createInvoice(receiveAmount, "payment");
+    setInvoiceForReceive(invoice?.invoice);
   }
 
   useEffect(()=>{
@@ -65,9 +97,46 @@ export default function Home() {
   }, []);
   
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>ATL BitLab Wallet</h1>
+    <div className="p-8">
+      <main className="w-full flex flex-col gap-8 row-start-2">
+        <h1 className="text-4xl">ATL BitLab Wallet</h1>
+
+        <h2 className="text-3xl">Balance</h2>
+        <p className="text-5xl">{balance} sats</p>
+
+        <h2 className="text-3xl">Federation</h2>
+        <input type="text" value={federationInvite} readOnly className="p-4 border border-gray-200" /> 
+
+        <h2 className="text-3xl">Send Bitcoin</h2>
+        <label>Amount</label>
+        <input type="number" value={sendAmount} onChange={handleAmountChange} className="p-4 border border-gray-200" />
+
+        <label>Lightning Invoice to Pay</label>
+        <input type="text" value={invoiceToPay} onChange={handleInvoiceChange} className="p-4 border border-gray-200" />
+
+        <button className="bg-orange-500 text-white p-6 font-bold" onClick={handleMakePayment}>Make Payment</button>
+
+        <div className="bg-gray-100 border border-gray-200 p-4 rounded-lg overflow-x-scroll">
+          {notesToSend}
+        </div>
+
+        <h2 className="text-3xl">Receive Bitcoin Over LN</h2>
+
+        <label>Amount</label>
+        <input type="number" value={receiveAmount} onChange={handleReceiveAmountChange} className="p-4 border border-gray-200" />
+
+        <button className="bg-orange-500 text-white p-6 font-bold" onClick={handleGetInvoice}>Get LN Invoice</button>
+
+        <div className="bg-gray-100 border border-gray-200 p-4 rounded-lg overflow-x-scroll">
+          {invoiceForReceive}
+        </div>
+
+        <h2 className="text-3xl">Receive Ecash</h2>
+
+        <label>Ecash to Redeem</label>
+        <input type="text" value={notesToReceive} onChange={handlesNotesChange} className="p-4 border border-gray-200" />
+        <button className="bg-orange-500 text-white p-6 font-bold" onClick={handleReceivePayment}>Receive Payment</button>
+        
       </main>
     </div>
   );
